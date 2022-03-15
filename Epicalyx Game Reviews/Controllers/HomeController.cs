6dd3feb2d10e26726eb9a91d10b1f6f655411716
George Jamieson <ac111480@avcol.school.nz>
2022-03-15 22:47:24 +1300

    Add project files.

---
+++ Epicalyx Game Reviews/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Epicalyx_Game_Reviews.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Epicalyx_Game_Reviews.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Epicalyx Game Reviews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Epicalyx_Game_Reviews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
