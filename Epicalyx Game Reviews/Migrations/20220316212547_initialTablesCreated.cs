4314fa5b9fa914598b7fded9363d206acb27f8a5
George Jamieson <ac111480@avcol.school.nz>
2022-03-17 10:50:19 +1300

    making all atributes required in the game table

---
+++ Epicalyx Game Reviews/Migrations/20220316212547_initialTablesCreated.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Epicalyx_Game_Reviews.Migrations
+{
+    public partial class initialTablesCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
